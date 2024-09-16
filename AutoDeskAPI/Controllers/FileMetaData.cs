0ea7d38181fd765979ee5b0f26caada3ab3589e9
bheema634 <swamy.net123@gmail.com>
2024-09-16 14:20:48 +0530

    Add project files.

---
+++ AutoDeskAPI/Controllers/FileMetaData.cs
@@ -0,0 +1,53 @@
+﻿using AutoDeskAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoDeskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileMetaData : ControllerBase
+    {
+        [HttpGet("filePath")]
+        public IActionResult GetFileInfo(string path)
+        {
+            //need to read the path from Directory
+          var exists =  Directory.Exists(path);
+            if (!exists)
+            {
+                return NotFound();
+            }
+
+            //1. create a service
+            List<FileMetaDataModel> filesMetaDataList = new List<FileMetaDataModel>();
+            try
+            {
+                //need to read the files from directory
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                   FileInfo fileInfo = new FileInfo(file);
+                    //meta data reading
+                    var metaData = new FileMetaDataModel
+                    {
+                        FileName = fileInfo.Name,
+                        FileExtension = fileInfo.Extension,
+                        SizeofFile = fileInfo.Length,
+                        CreationTime = fileInfo.CreationTime
+                    };
+                    filesMetaDataList.Add(metaData);
+                }
+                return Ok(filesMetaDataList);
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+    }
+}
---
+++ AutoDeskAPI/Model/FileMetaDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoDeskAPI.Model
+{
+    public class FileMetaDataModel
+    {
+        public string FileName { get; internal set; }
+        public string FileExtension { get; internal set; }
+        public long SizeofFile { get; internal set; }
+        public DateTime CreationTime { get; internal set; }
+    }
+}
